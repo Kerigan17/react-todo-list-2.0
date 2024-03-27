c7984f2987c7efb4a9b1775d24281b9d2ef001d0
Dima <dmitrymalko@gmail.com>
2024-03-27 20:43:30 +0300

    login completed

---
+++ frontend/src/components/App.jsx
@@ -0,0 +1,22 @@
+import {Navigate, Route, Routes, useNavigate} from "react-router-dom";
+import {useEffect, useState} from "react";
+import UserAuth from "./UserAuth/UserAuth";
+import TaskList from './TaskList/TaskList';
+
+export default function App(){
+    const navigate = useNavigate();
+    const [auth,setAuth] = useState(false)
+
+    useEffect(() => {
+        if(!auth) navigate('/auth');
+        else navigate('/');
+    }, [navigate, auth]);
+
+    return(
+        <Routes>
+            <Route path={'/'} element={<TaskList/>}/>
+            <Route path={'/auth'} element={<UserAuth onAuth = {setAuth}/>}/>
+            <Route path='*' element={<Navigate to='/' />} />
+        </Routes>
+    )
+}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import UserAuth from "./components/UserAuth/UserAuth";
+import {BrowserRouter} from "react-router-dom";
+import App from "./components/App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <UserAuth></UserAuth>
-  </React.StrictMode>
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>
 );
 
 reportWebVitals();
