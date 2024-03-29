7a5e34e8dcafa3364bfa74b633b0a59ce0949052
Dima <dmitrymalko@gmail.com>
2024-03-29 20:30:00 +0300

    all-tasks route works

--- backend/routes/tasks.mjs
+++ backend/routes/tasks.mjs
@@ -1,10 +1,13 @@
 import express from "express";
 import db from "../dbConnection.mjs";
+import {ObjectID} from "mongodb";
 const router = express.Router();
 
 router.get("/all-tasks", async (req, res) => {
     let collection = await db.collection("tasks");
     console.log(req.query.tasksIds);
+    for(let i = 0; i < req.query.tasksIds.length; i++)
+        req.query.tasksIds[i] = new ObjectID(req.query.tasksIds[i]);
     let result = await collection.find({_id:{$in: req.query.tasksIds}}).toArray();
 
     if (!result) res.send("Not found").status(404);
