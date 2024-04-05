1361bbcec4d78abd41fedc874ff964f6bc76ab45
Alexander Malko <alexsandr77867@gmail.com>
2024-04-05 20:34:06 +0400

    delete folders

--- frontend-ts/src/services/requestsDatabase.ts
+++
@@ -1,7 +0,0 @@
-const defaultUrl = 'http://localhost:8800/';
-
-export const getInfo = (url:string) =>
-    fetch(defaultUrl + url)
-        .then(response => response.json())
-        .then(data =>{return data})
-        .catch(error => console.error(error));
